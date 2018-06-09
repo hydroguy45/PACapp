730ea73c6d7be82435021f50ae52ccd7c3c81a89
hydroguy45 <foleychriscpfjr@gmail.com>
2018-06-09 15:25:29 -0400

    Parse works, need to properly format sheet to work with parser. Then I'll work with the dumping mechanism.

--- Backend/localCache.py
+++ Backend/localCache.py
@@ -1,31 +1,193 @@
 from __future__ import print_function
+from pprint import pprint
+from googleapiclient import discovery
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import json
-PACAppSheetUrl = "https://docs.google.com/spreadsheets/d/1vGwy/bPul8-9r-u10j_5sH06RxG-fqyNTTmcU7ajfBd0/edit#gid=0"
 
-#Setup the sheets API
+# Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
 	flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
 	creds = tools.run_flow(flow, store)
-service = build('sheets', 'v4', http=creds.authorize(Http()))
+service = discovery.build('sheets', 'v4', credentials=creds)
 
-# Call the sheets API
-SPREADSHEET_ID = '1vGwybPul8-9r-u10j_5sH06RxG-fqyNTTmcU7ajfBd0'
-RANGE_NAME = 'Group Credentials!A2:A5'
-result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
-values=result.get('values',[])
-if not values:
-	print('No data found.')
-else:
-	print('Name, Major:')
-	for row in values:
-		print('%s, %s' % (row[0], row[4]))
+spreadsheet_id = '1vGwybPul8-9r-u1Oj_5sHO6RxG-fqyNTTmcU7ajfBd0'  
 
+def getPACData():
+	print("[Attempting to load & Parse PAC data]")
+	PACSheet = "Group Credentials"
+	pacData = {}
+	#Get Subcommittee data
+	print("Loading subcommittee data")
+	subcomittees = {}
+	rows = getList(PACSheet,"A", "A",2)
+	for row in rows:
+		print("Looking at "+row[0])
+		subcomittees[row[0]] = getSubcommitteeData(row[0])
+	pacData["Subcomittees"] = subcomittees
+	#Announcements
+	print("Loading PAC wide announcements")
+	announcements = []
+	rows = getList(PACSheet,"C", "E", 5)
+	for row in rows:
+		announcement = {"Announcements":row[0], "Date":row[1], "Time":row[2]}
+		announcements.append(announcement)
+	pacData["Announcements"] = announcements
+	#General Performance Deadlines
+	print("Loading PAC wide deadlines")
+	deadlines = []
+	rows = getList(PACSheet,"G", "H", 3)
+	for row in rows:
+		deadline = {"What":row[0], "When":row[1]}
+		deadlines.append(deadline)
+	pacData["Deadlines"] = deadline
+	#Spaces
+	print("Loading location data")
+	spaces = {}
+	rows = getList(PACSheet,"J","J", 2)
+	for row in rows:
+		print("Looking at "+row[0])
+		spaces[row[0]] = getLocationData(row[0])
+	pacData["Spaces"] = spaces
+	#Done
+	pprint(pacData)
+	return pacData
+
+def getLocationData(name):
+	location = {}
+	#Announcements
+	print(" -Loading announcements")
+	announcements = []
+	rows = getList(name, "A","C",4)
+	for row in rows:
+		announcement = {"Announcement":row[0],"Date":row[1],"Time":row[2]}
+		announcements.append(announcement)
+	location["Announcements"] = announcements
+	#Deadlines
+	print(" -Loading deadlines")
+	deadlines = []
+	rows = getList(name,"E", "F", 3)
+	for row in rows:
+		deadline = {"What":row[0], "When":row[1]}
+		deadlines.append(deadline)
+	location["Deadlines"] = deadlines
+	return location
+
+def getSubcommitteeData(name):
+	subcomittee = {}
+	#Deadlines
+	print(" -Loading deadlines")
+	deadlines = []
+	rows = getList(name, "G", "H", 3)
+	for row in rows:
+		deadline = {"What":row[0],"When":row[1]}
+		deadlines.append(deadline)
+	subcomittee["Deadlines"] = deadlines
+	#Subcommittee Announcements
+	print(" -Loading announcements")
+	announcements = []
+	rows = getList(name, "C", "E", 4)
+	for row in rows:
+		announcement = {"Announcement":row[0], "Date":row[1], "Time":row[2]}
+		announcements.append(announcement)
+	subcomittee["Announcements"] = announcement
+	#Group data
+	print(" -Loading groups")
+	groups = []
+	rows = getList(name, "A", "A")
+	for row in rows:
+		print("   >Looking at "+row[0])
+		groups.append(getGroupData(row[0]))
+	subcomittee["Groups"] = groups
+	return subcomittee
+
+def getGroupData(name):
+	group = {}
+	#Add group members
+	persons = []
+	rows = getList(name, "A", "D")
+	for row in rows:
+		person = {"Name":row[0],"Position":row[1],"Email":row[2],"Expected Year of Graduation":row[3]}
+		persons.append(person)
+	group["ersons"] = persons
+	#Add group performance details
+	performanceDetails = {}
+	rows = getRange(name, "F", "G", 12, 20)
+	for row in rows:
+		performanceDetails[row[0]] = row[1]
+	group["Performance Details"] = performanceDetails
+	#Add group announcements
+	announcements = []
+	rows = getList(name, "F", "H", 26)
+	for row in rows:
+		announcement = {"Annoucement":row[0], "Date":row[1], "Time":row[2]};
+		announcements.append(announcement)
+	group["Announcements"] = announcements
+	#Add group demerits
+	demerits = []
+	rows = getRange(name, "F","H",3,10)
+	for row in rows:
+		demerit = {"Title":row[0], "Date":row[1],"Reason":row[2]}
+		demerits.append(demerit)
+	group["Demerits"] = demerits
+	return group
+
+def getRange(name, lowerEnd, higherEnd, startingRow, endingRow):
+	rows = []
+	range_ = name+'!'+lowerEnd+str(startingRow)+':'+higherEnd+str(endingRow) 
+	request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+	response = request.execute()
+	values = response.get('values', [])
+	if not values:
+		print('No data found.')
+	else:
+		print('Groups:')
+		print(len(values))
+		for row in values:
+			if(len(row) != 0):
+				numberOfColumns = ord(higherEnd[0]) -  ord(lowerEnd[0]) + 1
+				while(len(row) < numberOfColumns):
+					row.append("")
+				rows.append(row)
+	return rows
+
+def getList(name, lowerEnd, higherEnd, startingRow=2):
+	rows = []
+	rowLowerEnd = startingRow
+	gotAllTheNames = False
+	while(not gotAllTheNames):
+		rowHigherEnd = 29 + rowLowerEnd
+		range_ = name+'!'+lowerEnd+str(rowLowerEnd)+':'+higherEnd+str(rowHigherEnd) 
+		request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_)
+		response = request.execute()
+		#pprint(response)
+		values = response.get('values', [])
+		if not values:
+			print('     *No data found.')
+			gotAllTheNames = True
+		else:
+			#print('Groups:')
+			#print(len(values))
+			if len(values) != 30:
+				#print("Final buffer")
+				gotAllTheNames = True
+			else:
+				#print("Potentially more members left")
+				gotAllTheNames = False
+			for row in values:
+				if(len(row) != 0):
+					numberOfColumns = ord(higherEnd[0]) -  ord(lowerEnd[0]) + 1
+					while(len(row) < numberOfColumns):
+						row.append("")
+					rows.append(row)
+		rowLowerEnd = rowHigherEnd+1
+	return rows
+
+#TODO: once more sheets have been added, save this information to a json file
+getPACData()
 #def saveDictionary(dictionary, filename):
 #	f = open(filename, "w")
 #	json.dump(dictionary, f)
