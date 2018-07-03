eed1b82e78a9c464d1bf52443c38c4556515f460
hydroguy45 <foleychriscpfjr@gmail.com>
2018-07-03 18:54:42 -0400

    Finished necessary basic server functions.

--- Backend/server.py
+++ Backend/server.py
@@ -13,24 +13,38 @@ def loadJSON():
 	#print(json.dumps(cachedData["Subcomittees"]["ACK"]))
 
 def getPerformances():
-	#TODO: complete
-	return []
+	performances = []
+	subcomitteeDict = getGroups()
+	for subcomitteeName in subcomitteeDict:
+		for group in subcomitteeDict[subcomitteeName]:
+			performanceDetails = cachedData["Subcomittees"][subcomitteeName]["Groups"][group]["Performance Details"]
+			performance = {
+				"Description":performanceDetails["Description"],
+				"Title":performanceDetails["Performance title"],
+				"Cost":performanceDetails["Cost of tickets"],
+				"Family friendly?":performanceDetails["Family friendly?"],
+				"Date":performanceDetails["Date (mm/dd/yyyy)"],
+				"Location":performanceDetails["Location (as seen in sheet tag)"]
+			}
+			if(performance["Date"]!="" and performance["Location"]!=""):
+				performances.append(performance)
+	return performances
 
-def getPerformancesAsString():
-	return Response("TODO")
+def getPerformancesAsString(request):
+	return Response(json.dumps(getPerformances()))
 
 def getGroups():
 	allGroups = {}
-	for subcomitteeName in cacheData["Subcomittees"]:
-		subcomittee = cacheData["Subcomittees"][subcomitteeName]
+	for subcomitteeName in cachedData["Subcomittees"]:
+		subcomittee = cachedData["Subcomittees"][subcomitteeName]
 		groups = []
 		for groupName in subcomittee["Groups"]:
 			groups.append(groupName)
 		allGroups[subcomitteeName] = groups
 	return allGroups
 
-def getGroupsAsString():
-	return Response("TODO")
+def getGroupsAsString(request):
+	return Response(json.dumps(getGroups()))
 
 def getGroupInfo(subcomittee, groupname, password):
 	subcomittees = cachedData["Subcomittees"]
@@ -56,9 +70,20 @@ def getGroupInfo(subcomittee, groupname, password):
 		"Details":Details
 	}
 	return ResultData
-
+#Example
+#http://localhost:8080/getInfo?Subcomittee=ACK&Group=PennYo&Password=readthehandbook
 def getGroupInfoAsString(request):
-	return Response("TODO")
+	subcomittee = ""
+	group = ""
+	password = ""
+	if "Subcomittee" in request.GET and "Group" in request.GET and "Password" in request.GET:
+		subcomittee = request.GET["Subcomittee"]
+		group = request.GET["Group"]
+		password = request.GET["Password"]
+		info = getGroupInfo(subcomittee, group, password)
+		return Response(json.dumps(info))
+	else:
+		return Response("Illegal Argument")
 
 def main(request):
 	html = "<h1>The server is up and running</h1></br><p>%s</p>" % json.dumps(getGroupInfo("ACK","Pennsylvania Six-5000", "readthehandbook"))
@@ -71,6 +96,16 @@ if __name__ == "__main__":
 	with Configurator() as config:
 		config.add_route("main", "/")
 		config.add_view(main, route_name="main")
+
+		config.add_route("getInfo", "/getInfo")
+		config.add_view(getGroupInfoAsString, route_name="getInfo")
+
+		config.add_route("getGroups", "/getGroups")
+		config.add_view(getGroupsAsString, route_name="getGroups")
+
+		config.add_route("getPerformances", "/getPerformances")
+		config.add_view(getPerformancesAsString, route_name="getPerformances")
+
 		app=config.make_wsgi_app()
 	server = make_server(HOST, PORT, app)
 	print("Started server on %s:%d" % (HOST,PORT))
